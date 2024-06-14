3c870bd524af93d5f5bdc75eb2f71bd11bb951c6
Anushka <anushka30singh07@gmail.com>
2024-06-14 16:56:46 +0530

    database setup

---
+++ Backend/Database/db.js
@@ -0,0 +1,34 @@
+require ('dotenv').config();
+const mongoose = require("mongoose");
+const { type } = require('os');
+
+mongoose.connect (process.env.url);
+
+const UserSchema = mongoose.Schema({
+    userName:{
+        type:String,
+        reqired:true,
+        unique:true,
+        lowercase:true
+
+    },
+    password:{
+        type : String,
+        minLength: 8,
+        maxLength:100,
+        require:true
+    } ,
+    firstName:{
+        type:String,
+        require:true
+
+    } ,
+    lastName: {
+        type : String
+    } 
+})
+const User = mongoose.model("Users" , UserSchema);
+
+model.exports = {
+    Users
+}
