b58eb8fe8ac5f531f36f3805cdb79c0480b67a91
Anushka <anushka30singh07@gmail.com>
2024-06-16 11:57:16 +0530

    update

--- Frontend/src/components/LandingPage.jsx
+++ Frontend/src/components/LandingPage.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import image from '../assets/E-Wallet.png';
+import './LandingPage.css';
 const LandingPage = () => {
   return (
     <div className="container mx-auto p-4">
@@ -12,8 +13,8 @@ const LandingPage = () => {
           />
         </div>
         <div className="w-full lg:w-1/2 mt-4 lg:mt-0 lg:pl-8 text-center lg:text-left">
-          <h1 className="text-2xl lg:text-6xl font-bold mb-4 my-56 ">Welcome to FlexiWallet</h1>
-          <p className="text-lg lg:text-2xl mb-4 my-48 mt-10">
+          <h1 className="text-2xl lg:text-6xl font-bold mb-4 my-56 animate-fade-in ">Welcome to FlexiWallet</h1>
+          <p className="text-lg lg:text-2xl mb-4 my-48 mt-10 animate-fade-in">
           FlexiWallet is a cutting-edge digital wallet solution designed to streamline your financial transactions.
           </p>
           <hr className="my-6 border-gray-300" />
